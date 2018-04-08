5d4b8cf58eda296bc586d735fff270c4c05d2ec5
Alex811 <811alex@gmail.com>
2018-04-08 16:12:15 +0300

    Add files via upload

---
+++ test.bat
@@ -0,0 +1,25 @@
+:: This script can be used to test elevateMe.vbs
+:: try executing it directly (using a normal user) and then through elevateMe.vbs
+:: the first one should fail, while the latter should succeed.
+::
+:: It changes the start type of the service "TabletInputService", which can only be done 
+:: with elevated privileges. The default value in Windows 10 is auto (2).
+:: Then it prints any arguments it was executed with, so you can test those too.
+
+@echo off
+echo This script will only run successfully if ran with elevated privileges!
+call:showValue
+echo. & echo Attempting to change to "demand"...
+sc config TabletInputService start=demand
+call:showValue
+echo. & echo Attempting to change to "auto"...
+sc config TabletInputService start=auto
+call:showValue
+echo. & echo Arguments: %*
+pause
+exit /b
+
+:showValue
+for /F "tokens=4" %%i in ('sc qc TabletInputService ^| findstr START_TYPE') do set outp=%%i
+echo. & echo Current value: %outp%
+exit /b
